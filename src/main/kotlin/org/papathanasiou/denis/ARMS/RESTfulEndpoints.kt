7408111beb793ce06620b8b1072f19888d6331d0
Denis Papathanasiou <denis@papathanasiou.org>
2017-06-03 17:25:34 -0400

    Added custom exception for responding to invalid api requests as 'Bad Request' (400)

--- src/main/kotlin/org/papathanasiou/denis/ARMS/RESTfulEndpoints.kt
+++ src/main/kotlin/org/papathanasiou/denis/ARMS/RESTfulEndpoints.kt
@@ -2,6 +2,11 @@ package org.papathanasiou.denis.ARMS
 
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
+import javax.ws.rs.core.Response
+
+class BadAPIRequest : WebApplicationException {
+    constructor(message: String?) : super(message, Response.Status.BAD_REQUEST)
+}
 
 @Path("")
 class RESTfulEndpoints {
