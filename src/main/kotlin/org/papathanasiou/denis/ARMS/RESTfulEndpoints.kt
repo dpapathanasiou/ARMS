c6533b425ff7030354e162485896b73d11f6bd68
Denis Papathanasiou <denis@papathanasiou.org>
2017-06-04 13:03:59 -0400

    Switched to returning a Response object from the endpoints, which gives more control over appropriate http codes

--- src/main/kotlin/org/papathanasiou/denis/ARMS/RESTfulEndpoints.kt
+++ src/main/kotlin/org/papathanasiou/denis/ARMS/RESTfulEndpoints.kt
@@ -6,6 +6,7 @@ import javax.ws.rs.core.MediaType.APPLICATION_JSON
 import javax.ws.rs.core.MultivaluedMap
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.UriInfo
+import kotlin.reflect.jvm.internal.impl.resolve.scopes.SyntheticScopes
 
 class BadAPIRequest : WebApplicationException {
     constructor(message: String?) : super(message, Response.Status.BAD_REQUEST)
@@ -14,6 +15,8 @@ class BadAPIRequest : WebApplicationException {
 @Path("")
 class RESTfulEndpoints(config: ARMSConfiguration): MongoInterface {
     override val connection: MongoConnection = MongoConnection(config.mongoURI)
+
+    val EMPTY_JSON = "{}"
 
     fun scalarizeQueryParameters(query: MultivaluedMap<String, String>): Map<String,String> {
         return query.filter({it.value.isNotEmpty()})
@@ -25,12 +28,16 @@ class RESTfulEndpoints(config: ARMSConfiguration): MongoInterface {
     @Path("{database}/{collection}")
     fun findDocument(@PathParam("database") database: String,
                      @PathParam("collection") collection: String,
-                     @Context ui: UriInfo): String? {
+                     @Context ui: UriInfo): Response {
 
         val query = ui.getQueryParameters()
         if( query.isEmpty() ) throw BadAPIRequest("please provide query parameters")
 
         val results = getDocuments(database, collection, scalarizeQueryParameters(query))
-        return results?.joinToString(",") ?: "{}"
+        val matches = results?.size ?: 0
+        val content = if( matches > 0 ) results?.joinToString(",") else EMPTY_JSON
+        val status  = if( matches > 0 ) Response.Status.OK else Response.Status.NOT_FOUND
+
+        return Response.status(status).type(APPLICATION_JSON).entity(content).build()
     }
 }
