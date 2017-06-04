50b6fb68025d46230ee9c370d2b95191b5de6471
Denis Papathanasiou <denis@papathanasiou.org>
2017-06-03 20:53:54 -0400

    Added check for query parameters, invoking BadAPIRequest if missing

--- src/main/kotlin/org/papathanasiou/denis/ARMS/RESTfulEndpoints.kt
+++ src/main/kotlin/org/papathanasiou/denis/ARMS/RESTfulEndpoints.kt
@@ -1,8 +1,10 @@
 package org.papathanasiou.denis.ARMS
 
 import javax.ws.rs.*
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
 import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriInfo
 
 class BadAPIRequest : WebApplicationException {
     constructor(message: String?) : super(message, Response.Status.BAD_REQUEST)
@@ -15,7 +17,12 @@ class RESTfulEndpoints(config: ARMSConfiguration): MongoInterface {
     @GET @Produces(APPLICATION_JSON)
     @Path("{database}/{collection}")
     fun findDocument(@PathParam("database") database: String,
-                     @PathParam("collection") collection: String): String? {
+                     @PathParam("collection") collection: String,
+                     @Context ui: UriInfo): String? {
+
+        val query = ui.getQueryParameters()
+        if( query.isEmpty() ) throw BadAPIRequest("please provide query parameters")
+
         // simple reply for now
         return "{'db': '$database', 'coll': '$collection'}"
     }
